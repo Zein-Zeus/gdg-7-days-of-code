eec9f11dfbc8fbd43636a350a367034333b86db1
Zenia N <123252432+Zein-Zeus@users.noreply.github.com>
2024-11-20 20:19:06 +0530

    Create Solution.cpp (Zenia)

---
+++ DAY 3/Solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    for(int i = a; i<=b; i++){
+        if(i%5 == 0){
+            if(i%7 == 0){
+                cout << "FooBar" << endl;
+            }
+            else {
+                cout << "Foo" << endl;
+            }
+        }
+        else {
+            if(i%7 == 0){
+                cout << "Bar" << endl;
+            }
+            else {
+                cout << "Baz" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
